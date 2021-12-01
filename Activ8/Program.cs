6977449b1297ea69d057f512f22ef3bad6481854
Bruce <Bruce@DESKTOP-O337CUV>
2021-12-01 23:04:08 +0000

    Add project files.

---
+++ Activ8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ActivationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine(@"Activ8 - by Bruce Tonge and John Cooke
+
+                Gets conformationID from Microsoft Activation. 
+
+                Usage: Activ8.exe <InstalationID> <ExtendedProductID>");
+            }
+            else
+            {
+                string InstallationId = args[0];
+                string ExtendedProductId = args[1];
+
+                var result = ActivationHelper.CallWebService(1, InstallationId, ExtendedProductId);
+                Console.WriteLine(result);          
+            }
+            
+        }
+    }
+}
+
